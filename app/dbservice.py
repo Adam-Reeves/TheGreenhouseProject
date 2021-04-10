d3fd6ecccf0764047911aa3a6896474626d96548
Adam <adam.j.reeves@outlook.com>
2021-04-10 15:30:31 +0100

    chore: change db lib

--- app/dbservice.py
+++ app/dbservice.py
@@ -1,18 +1,27 @@
-import postgresql
+import psycopg2
 
 from dotenv import dotenv_values    
 
 config = dotenv_values(".env")
 
 def get_db_connection():
-    connection_string = config["DB_CONNECTION_STRING"]
-    db = postgresql.open(connection_string)
-    return db
+    conn = psycopg2.connect(config["DB_CONNECTION_STRING"], sslmode='require')
+    return conn
 
-def setup_db(db_connection):
-    db_connection.execute("CREATE TABLE greenhouse (image bytea, image_extension text)")
+def setup_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE greenhouse (image bytea, image_extension text)")
+    conn.commit()
+    conn.close()
+    cur.close()
 
-def insert_image_into_db(db, image, image_extension):
-    insert_image_query = db.prepare("INSERT INTO greenhouse VALUES ($1, $2)")
-    with db.xact():
-        insert_image_query(bytes("test_1", 'utf-8'), "test_2")
+def insert_image_into_db(image, image_extension):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO greenhouse VALUES (%s, %s)",
+      (image, image_extension))
+    conn.commit()
+    conn.close()
+    cur.close()
+
--- app/entrypoint.py
+++ app/entrypoint.py
@@ -1,9 +1,7 @@
 from dbservice import *
 from pathlib import Path
 
-db_conn = get_db_connection()
-
 path = Path(__file__).parent / "sample_img.txt"
 with path.open() as f:
     image_str = f.readlines()
-    insert_image_into_db(db_conn, "test", "jpg")
+    insert_image_into_db("test", "jpg")
